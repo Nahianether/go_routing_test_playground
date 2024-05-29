c7f4dfe5f8df5dfa62c9d0552c58f0705066e91b
Nahianether <nahianether3@gmail.com>
2024-05-29 19:52:11 +0600

    isar remove from code

--- lib/src/theme/provider/theme.provider.dart
+++ lib/src/theme/provider/theme.provider.dart
@@ -1,11 +1,5 @@
-import 'package:flutter/foundation.dart';
-
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../../db/isar.dart';
 import '../../db/paths.dart';
 import '../../modules/setting/model/setting.model.dart';
-import '../../modules/setting/provider/settings.provider.dart';
 import '../model/theme.model.dart';
 
 // typedef ThemeNotifier = NotifierProvider<ThemeProvider, ThemeProfile>;
